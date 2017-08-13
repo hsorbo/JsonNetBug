69161d10585b13fec76215238f458e57eb787486
Håvard Sørbø <havard@hsorbo.no>
2017-08-13 14:36:37 +0200

    initial

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open Newtonsoft.Json
+type X ={ Y:decimal }
+
+[<EntryPoint>]
+let main argv =
+    let original = Some {Y = 79228162477370849463M}
+    let serialized = JsonConvert.SerializeObject(original)
+    let deserialized = JsonConvert.DeserializeObject<X option>(serialized)
+    printfn "%A" serialized
+    printfn "%A" deserialized
+    printfn "%A"  (deserialized = original)
+    0 
